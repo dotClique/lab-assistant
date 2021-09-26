0d6c052c5fca9df34635548ef7e3951f11a516bf
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-09-26 22:41:38 +0200

    Adapt sample usage to i3/create-sample-site

--- project-2/src/App.tsx
+++ project-2/src/App.tsx
@@ -1,23 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Header from "./components/Header";
 import Tab from './components/Tab';
 import Info from './components/Info';
-import {Commit, getCommits} from "./api/Commits";
-import {Award, getAwards} from "./api/Awards";
 
 function App() {
-  const [{commits, awards}, setState] = useState<{ commits: Commit[], awards: Award[] }>({commits: [], awards: []});
-  useEffect(() => {
-      getCommits().then(commits => setState(prev => ({...prev, commits: commits})));
-      getAwards().then(awards => setState(prev => ({...prev, awards: awards})));
-  }, [])
   return (
     <div>
       <Header />
       <Info />
       <Tab />
-      <ul>{commits.map(c => <li>{c.title}</li>)}</ul>
-      <ul>{awards.map(a => <li>{a.name}</li>)}</ul>
     </div>
   );
 }
--- project-2/src/components/Tab.tsx
+++ project-2/src/components/Tab.tsx
@@ -1,10 +1,18 @@
 import { Tabs } from 'antd';
 import '../styles/Tab.css';
+import React, {useEffect, useState} from "react";
+import {Commit, getCommits} from "../api/Commits";
+import {Award, getAwards} from "../api/Awards";
 
 
 const { TabPane } = Tabs;
 
 function Tab() {
+    const [{commits, awards}, setState] = useState<{ commits: Commit[], awards: Award[] }>({commits: [], awards: []});
+    useEffect(() => {
+        getCommits().then(commits => setState(prev => ({...prev, commits: commits})));
+        getAwards().then(awards => setState(prev => ({...prev, awards: awards})));
+    }, [])
     return (
         <div className="card-container">
             <Tabs type="card">
@@ -15,13 +23,11 @@ function Tab() {
                 </TabPane>
                 <TabPane tab="Commits" key="2">
                     <p>Here you can see commits for the project</p>
-                    <p>More content</p>
-                    <p>More content</p>
+                    <ul>{commits.map(c => <li>{c.title}</li>)}</ul>
                 </TabPane>
-                <TabPane tab="Repository files" key="3">
-                    <p>Here you can see the repository files in the project</p>
-                    <p>More content</p>
-                    <p>More content</p>
+                <TabPane tab="Awards" key="3">
+                    <p>Here you can see the awards used in the project</p>
+                    <ul>{awards.map(a => <li>{a.name}</li>)}</ul>
                 </TabPane>
             </Tabs>
         </div>
