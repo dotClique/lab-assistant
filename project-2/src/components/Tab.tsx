867d53047f973535f4cbe0acc8ce1d2a9f7c70e1
techiag <55250890+techiag@users.noreply.github.com>
2021-09-26 15:00:01 +0200

    Add sample text to tabs

--- project-2/src/components/Tab.tsx
+++ project-2/src/components/Tab.tsx
@@ -9,21 +9,19 @@ function Tab() {
         <div className="card-container">
             <Tabs type="card">
                 <TabPane tab="Issues" key="1">
-                    <p>Content of Tab Pane 1</p>
-                    <p>Content of Tab Pane 1</p>
-                    <p>Content of Tab Pane 1</p>
-                    <p>rter</p>
-                    <h2>hello</h2>
+                    <p>Here you can see issues for the project</p>
+                    <p>More content</p>
+                    <p>More content</p>
                 </TabPane>
                 <TabPane tab="Commits" key="2">
-                    <p>Content of Tab Pane 2</p>
-                    <p>Content of Tab Pane 2</p>
-                    <p>Content of Tab Pane 2</p>
+                    <p>Here you can see commits for the project</p>
+                    <p>More content</p>
+                    <p>More content</p>
                 </TabPane>
                 <TabPane tab="Repository files" key="3">
-                    <p>Content of Tab Pane 3</p>
-                    <p>Content of Tab Pane 3</p>
-                    <p>Content of Tab Pane 3</p>
+                    <p>Here you can see the repository files in the project</p>
+                    <p>More content</p>
+                    <p>More content</p>
                 </TabPane>
             </Tabs>
         </div>
