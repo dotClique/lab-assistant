7658a872b40ea823d70d9b2382e71e80baad4406
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-09-26 23:11:37 +0200

    Touch up visuals

--- project-2/src/components/Header.tsx
+++ project-2/src/components/Header.tsx
@@ -1,11 +1,11 @@
-import logo from "../gitlab-logo.png";
+import logo from "../gitlab-logo-w.png";
 import "../styles/Header.css";
 
 function Header() {
     return(
-        <div className="Header">
+        <div className="header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 id="title">Gitlab-repo Information</h1>
+            <h1 className={"header-text"}>Gitlab-repo Information</h1>
         </div>
     );
 }
