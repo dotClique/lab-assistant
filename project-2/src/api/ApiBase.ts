6cf3799c146237c4aa5f1f4e2e2becf4126068f9
Lars Mitsem Selbekk <lars@selbekk.com>
2021-09-26 22:01:07 +0200

    Remove API proxy
    Somehow, CORS has been enabled the whole time, and for some reason it
    didn't work until now, but now it does. Strange.

--- project-2/src/api/ApiBase.ts
+++ project-2/src/api/ApiBase.ts
@@ -2,12 +2,11 @@ import {AxiosRequestConfig} from "axios";
 
 const gitlabInstanceUrl = "https://gitlab.stud.idi.ntnu.no";
 const apiVersion = "v4"
-const proxyUrl = "http://localhost:3123";
 const projectId = encodeURIComponent("it2810-h21/team-25/project-2");
 
 export const axiosConfig: AxiosRequestConfig = {
     headers: {
         "PRIVATE-TOKEN": "ZTziure4DK-wh_Z8Fmwq",
     },
-    baseURL: `${proxyUrl}/${gitlabInstanceUrl}/api/${apiVersion}/projects/${projectId}`,
+    baseURL: `${gitlabInstanceUrl}/api/${apiVersion}/projects/${projectId}`,
 };
