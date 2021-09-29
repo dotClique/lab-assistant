02d07f76d2815f6c4de279afaab27005a87884e8
techiag <55250890+techiag@users.noreply.github.com>
2021-09-29 12:22:12 +0200

    Visualize textual data in a nicer way

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,4 +1,4 @@
-import {Tabs} from 'antd';
+import {Card, List, Tabs} from 'antd';
 import '../styles/Tab.css';
 import React, {useContext, useEffect, useState} from "react";
 import {Commit, getCommits} from "../api/Commits";
@@ -34,32 +34,54 @@ function Tab() {
         <div className="card-container">
             <Tabs>
                 <TabPane tab="Issues" key="1">
-                    <p>Here you can see issues for the project</p>
-                    <ul>
+                    <p>Here you can see issues for the project</p><br/>
+                    <div>
                         {issues.map(i =>
-                            <li key={i.iid}>
-                                {i.title}
-                                <ul>
-                                    <li key={1}>Closed: {i.closed_at === null ? "❌" : "✔"}</li>
-                                    <li key={2}>Time spent: {Math.round(i.time_stats.total_time_spent / 3600)}h</li>
-                                    <li key={3}>Author: {anonymize(names, i.author.id)}</li>
-                                    <li key={4}>Upvotes: {i.upvotes}</li>
-                                </ul>
-                            </li>
+                            <div>
+                                <Card title={i.title} bordered={true} style={{ width: "45%" }}>
+                                    <p><strong>Closed:</strong> {i.closed_at === null ? "❌" : "✔"}</p>
+                                    <p><strong>Time spent:</strong> {Math.round(i.time_stats.total_time_spent / 3600)}h</p>
+                                    <p><strong>Author:</strong> {anonymize(names, i.author.id)}</p>
+                                    <p><strong>Upvotes:</strong> {i.upvotes}</p>
+                                </Card>
+                            <br/>
+                            </div>
                         )}
-                    </ul>
+                    </div>
                 </TabPane>
                 <TabPane tab="Commits" key="2">
-                    <p>Here you can see commits for the project</p>
-                    <ul>{commits.map(c => <li key={c.id}>{c.title}</li>)}</ul>
+                    <p>Here you can see commits for the project</p><br/>
+                    <List
+                        style={{width: "45%"}}
+                        size="large"
+                        header={<div style={{fontSize: 20}}><strong>Project commits</strong></div>}
+                        bordered
+                        dataSource={commits}
+                        renderItem={item => <List.Item>{item.title}</List.Item>}
+                    />
                 </TabPane>
                 <TabPane tab="Awards" key="3">
-                    <p>Here you can see the awards used in the project</p>
-                    <ul>{awards.map(a => <li key={a.id}>{a.name}</li>)}</ul>
+                    <p>Here you can see the awards used in the project</p><br/>
+                    <div>
+                        {awards.map(a => 
+                            <div>
+                                <Card title={a.name} bordered={true} style={{ width: "45%" }}>
+                                    <p>Display actual emoji here</p>
+                                </Card>
+                                <br/>
+                            </div>
+                        )}
+                    </div>
                 </TabPane>
             </Tabs>
         </div>
     )
 }
+
+/*
+    Previous solutions without nice visualization:
+    <ul>{commits.map(c => <li key={c.id}>{c.title}</li>)}</ul>
+    <ul>{awards.map(a => <li key={a.id}>{a.name}</li>)}</ul>
+*/
 
 export default Tab;
