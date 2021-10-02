62105f3f4e918f4c7a32c2749f695316b752eb27
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-10-02 14:29:40 +0200

    Add additions/deletions to commits data, with multi-axis chart

--- src/api/Commits.ts
+++ src/api/Commits.ts
@@ -14,11 +14,16 @@ export interface Commit {
     committer_email: string
     committed_date: string
     web_url: string
+    stats: {
+        additions: number,
+        deletions: number
+        total: number
+    }
 }
 
 export async function getCommits(accessToken: string, projectId: string): Promise<Commit[]> {
     return (
-        getAllPages<Commit[]>(`/repository/commits`, axiosConfig(accessToken, projectId, 100))
+        getAllPages<Commit[]>(`/repository/commits?with_stats=true`, axiosConfig(accessToken, projectId, 100))
     ).then(res => res.map(page => page.data).flat()).catch(e => {
         console.error("Failed to retrieve commits", e);
         return [];
--- src/components/CommitsTab.tsx
+++ src/components/CommitsTab.tsx
@@ -6,13 +6,23 @@ import {Line} from "react-chartjs-2";
 import '../styles/CommitsTab.css';
 import {anonymizeString} from "../api/Users";
 
+interface DateStat {
+    commits: number,
+    additions: number,
+    deletions: number
+}
+
 export default function CommitsTab() {
 
     const auth = useContext(AuthContext);
-    const [{commits, dates, datesTally}, setState] = useState<{ commits: Commit[], dates: string[], datesTally: number[] }>({
+    const [{
+        commits,
+        dates,
+        dateStats
+    }, setState] = useState<{ commits: Commit[], dates: string[], dateStats: DateStat[] }>({
         commits: [],
         dates: [],
-        datesTally: []
+        dateStats: []
     });
 
     const assets = useContext(AssetsContext);
@@ -34,15 +44,24 @@ export default function CommitsTab() {
             // Don't update if the component has unmounted
             if (!isActive) return;
             // Get all ISO date strings from date of first commit to current date
-            const minDate = new Date(Math.min.apply(Math, commits.map(function(o) { return Date.parse(o.created_at.slice(0,10)); })))
+            const minDate = new Date(Math.min.apply(Math, commits.map(function (o) {
+                return Date.parse(o.created_at.slice(0, 10));
+            })))
             const maxDate = new Date()
-            const activeDates: string[] = getDatesBetweenDates(minDate, maxDate).map(d => d.toISOString().slice(0,10))
-            // Count commits per date
-            const datesTally = new Array(activeDates.length).fill(0);
+            const activeDates: string[] = getDatesBetweenDates(minDate, maxDate).map(d => d.toISOString().slice(0, 10))
+            // Count commits, additions and deletions per date
+            const dateStats = Array.from(new Array(activeDates.length)).map(() => ({
+                commits: 0,
+                additions: 0,
+                deletions: 0
+            }));
             for (const i of commits) {
-                datesTally[activeDates.indexOf(i.created_at.slice(0,10))] += 1
+                const dateIndex = activeDates.indexOf(i.created_at.slice(0, 10))
+                dateStats[dateIndex].commits += 1
+                dateStats[dateIndex].additions += i.stats.additions
+                dateStats[dateIndex].deletions -= i.stats.deletions
             }
-            setState(prev => ({...prev, commits: commits, dates: activeDates, datesTally: datesTally}));
+            setState(prev => ({...prev, commits: commits, dates: activeDates, dateStats: dateStats}));
         })
         return () => {
             isActive = false;
@@ -75,32 +94,62 @@ export default function CommitsTab() {
                         labels: dates,
                         datasets: [
                             {
+                                type: 'line',
+                                yAxisID: "y1",
                                 label: '# of commits',
-                                data: datesTally,
+                                data: dateStats.map(s => s.commits),
                                 fill: false,
                                 backgroundColor: theme === "orange" ? 'rgb(255, 85, 0)' : 'rgb(63, 140, 228)',
-                                borderColor: theme === "orange" ? 'rgb(255, 85, 0, 0.4)' : 'rgba(63, 140, 228, 0.4)'
+                                borderColor: theme === "orange" ? 'rgb(255, 85, 0, 0.8)' : 'rgba(63, 140, 228, 0.8)'
+                            },
+                            {
+                                type: 'bar',
+                                yAxisID: "y2",
+                                label: 'additions',
+                                data: dateStats.map(s => s.additions),
+                                backgroundColor: 'rgba(82,184,122,0.6)'
+                            },
+                            {
+                                type: 'bar',
+                                yAxisID: "y2",
+                                label: 'deletions',
+                                data: dateStats.map(s => s.deletions),
+                                backgroundColor: 'rgba(236,89,65,0.6)'
                             },
                         ],
                     }}
                     options={{
                         maintainAspectRatio: false,
                         scales: {
-                            y: {
-                                ticks: {
-                                    stepSize: 1
+                            y1: {
+                                type: 'linear',
+                                position: 'left',
+                                title: {
+                                    display: true,
+                                    text: 'Commits per day',
+                                    font: {
+                                        size: 18
+                                    }
                                 }
-                            }
+                            },
+                            y2: {
+                                type: 'linear',
+                                position: 'right',
+                                title: {
+                                    display: true,
+                                    text: 'Additions',
+                                    font: {
+                                        size: 18
+                                    }
+                                }
+                            },
                         },
                         plugins: {
-                            legend: {
-                                display: false
-                            },
                             title: {
                                 display: true,
                                 text: 'Commits over time',
                                 font: {
-                                    size: 18
+                                    size: 22
                                 },
                                 padding: {
                                     top: 10,
