3ef4f6925c465022c25dd0acb38988506b64af0b
techiag <55250890+techiag@users.noreply.github.com>
2021-10-03 16:45:43 +0200

    Add documentation to Users.ts

--- src/api/Users.ts
+++ src/api/Users.ts
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import {AssetsContextType} from "../App";
 
+// Define the types of a User
 export interface User {
     id: number,
     name: string,
@@ -10,6 +11,7 @@ export interface User {
     // web_url: string,
 }
 
+// Get all possible names a user can have from text files
 export async function getNames() {
     return {
         nouns: (await Axios.get<string>("/nouns.txt")).data.split("\n"),
@@ -17,10 +19,12 @@ export async function getNames() {
     }
 }
 
+// Function that anonymizes a username based on its user id
 export function anonymize({nouns, adjectives}: {nouns: string[], adjectives: string[]}, userId: number): string {
     return adjectives[userId % adjectives.length] + " " + nouns[userId % nouns.length]
 }
 
+// Function that uses a hashcode to anonymize a string
 export function anonymizeString({nouns, adjectives}: AssetsContextType["names"], str: string): string {
     const hashCode = Math.abs(str.split('').reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0)) % 10000
     return adjectives[hashCode % adjectives.length] + " " + nouns[hashCode % nouns.length]
