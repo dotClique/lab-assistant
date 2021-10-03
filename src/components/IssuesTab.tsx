31205145417a0a31c708b29fd51ae6a3fdd00f78
Lars Mitsem Selbekk <lars@selbekk.com>
2021-10-03 03:38:37 +0200

    Allow filtering issues on label(s)

--- src/components/IssuesTab.tsx
+++ src/components/IssuesTab.tsx
@@ -1,28 +1,40 @@
 import React, {useContext, useEffect, useState} from "react";
 import {AuthContext, AssetsContext, ThemeContext} from "../App";
 import {getIssues, Issue, IssueState} from "../api/Issues";
-import {Card, Radio, RadioChangeEvent, Space, Tag, Typography} from "antd";
+import {Card, Radio, RadioChangeEvent, Select, Space, Tag, Typography} from "antd";
 import {anonymize} from "../api/Users";
 import {CheckCircleTwoTone, ClockCircleTwoTone} from '@ant-design/icons';
 import {Radar} from "react-chartjs-2";
 import '../styles/IssuesTab.css'
 
 const {Text} = Typography;
+const {Option} = Select;
 
 interface IssuesTabState {
     issues: Issue[],
     weekdayCreatedTally: number[],
     weekdayClosedTally: number[],
-    issueStatusFilter?: IssueState,
+    issueStateFilter?: IssueState,
+    issuesLabelFilter: string[],
+    labels: string[],
 }
 
 export default function IssuesTab() {
 
     const auth = useContext(AuthContext);
-    const [{issues, weekdayCreatedTally, weekdayClosedTally, issueStatusFilter}, setState] = useState<IssuesTabState>({
+    const [{
+        issues,
+        weekdayCreatedTally,
+        weekdayClosedTally,
+        issueStateFilter,
+        issuesLabelFilter,
+        labels
+    }, setState] = useState<IssuesTabState>({
         issues: [],
         weekdayCreatedTally: [],
         weekdayClosedTally: [],
+        issuesLabelFilter: [],
+        labels: [],
     });
 
     const assets = useContext(AssetsContext);
@@ -56,6 +68,23 @@ export default function IssuesTab() {
         };
     }, [auth])
 
+    useEffect(() => {
+        const distinctLabels = (issues: Issue[]) => {
+            const n: string[] = [];
+            for (const issue of issues) {
+                for (const label of issue.labels) {
+                    if (!n.includes(label)) {
+                        n.push(label);
+                    }
+                }
+            }
+            return n;
+        }
+        setState(prev => ({
+            ...prev, labels: distinctLabels(prev.issues)
+        }))
+    }, [issues]);
+
     const {theme} = useContext(ThemeContext)
 
     const radioOptions: { label: string, value: IssueState | "" }[] = [
@@ -64,10 +93,21 @@ export default function IssuesTab() {
         {label: "Open", value: "opened"},
     ];
 
-    const updateFilter = (e: RadioChangeEvent) => {
+    const updateStateFilter = (e: RadioChangeEvent) => {
         const newValue = e.target.value === "" ? null : e.target.value;
-        setState(prev => ({...prev, issueStatusFilter: newValue}));
+        setState(prev => ({...prev, issueStateFilter: newValue}));
     };
+
+    const updateLabelFilter = (value: string[]) => {
+        setState(prev => ({...prev, issuesLabelFilter: value}));
+    };
+
+    const addLabelToFilter: React.MouseEventHandler<HTMLSpanElement> = e => {
+        const label = e.currentTarget.innerText;
+        if (!issuesLabelFilter.includes(label)) {
+            setState(prev => ({...prev, issuesLabelFilter: [...prev.issuesLabelFilter, label]}))
+        }
+    }
 
     return (
         <div className={"tab-content"}>
@@ -75,10 +115,17 @@ export default function IssuesTab() {
                 <div>
                     <Space direction="vertical" className="noteable-filter-container">
                         <Text strong={true}>Show issues that are:</Text>
-                        <Radio.Group onChange={updateFilter} size="large" options={radioOptions} defaultValue=""
+                        <Radio.Group onChange={updateStateFilter} size="large" options={radioOptions} defaultValue=""
                                      optionType="button" buttonStyle="solid"/>
+                        <Select style={{minWidth: "20em", maxWidth: "100%", overflow: "visible"}} mode="multiple" allowClear
+                                value={issuesLabelFilter} placeholder="Labels to filter on"
+                                onChange={updateLabelFilter}>{labels.map(l => <Option key={l} value={l}>{l}</Option>)}</Select>
                     </Space>
-                    {(issueStatusFilter == null ? issues : issues.filter(i => i.state === issueStatusFilter)).map(i =>
+                    {((issueStateFilter == null && issuesLabelFilter.length === 0) ?
+                        issues : issues.filter(i =>
+                            (issueStateFilter == null || i.state === issueStateFilter)
+                            && (issuesLabelFilter.length === 0 || issuesLabelFilter.some(label => i.labels.includes(label)))
+                        )).map(i =>
                         <div key={i.iid}>
                             <Card
                                 title={
@@ -93,7 +140,7 @@ export default function IssuesTab() {
                                         <br/>
                                         {i.labels.length > 0 &&
                                         i.labels.map(l => (
-                                            <Tag key={l}>{l}</Tag>
+                                            <Tag onClick={addLabelToFilter} key={l}>{l}</Tag>
                                         ))
                                         }
                                     </>
